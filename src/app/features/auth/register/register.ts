db72dfe8c1081c5c47896b8258219537eb20de03
Diego-monfort-landot <diegomonfort1989@gmail.com>
2026-06-30 23:02:33 +0200

    Add register page with validations

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.html',
-  styleUrl: './register.scss',
+  styleUrl: './register.scss'
 })
 export class Register {
+  name = '';
+  email = '';
+  password = '';
+  acceptedTerms = false;
+  successMessage = '';
 
+  onSubmit(): void {
+    this.successMessage = 'Registrierung erfolgreich simuliert. Du kannst dich jetzt einloggen.';
+    this.name = '';
+    this.email = '';
+    this.password = '';
+    this.acceptedTerms = false;
+  }
 }
