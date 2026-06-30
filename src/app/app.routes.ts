854c645de827b39e321dd6e374dc702ac09846bc
Diego-monfort-landot <diegomonfort1989@gmail.com>
2026-06-30 21:27:03 +0200

    Initial Angular project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './features/home/home';
+import { About } from './features/about/about';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { Dashboard } from './features/dashboard/dashboard';
+import { Contact } from './features/contact/contact';
+
+export const routes: Routes = [
+  { path: '', component: Home, title: 'FitStride | Home' },
+  { path: 'about', component: About, title: 'Über mich | FitStride' },
+  { path: 'login', component: Login, title: 'Login | FitStride' },
+  { path: 'register', component: Register, title: 'Registrierung | FitStride' },
+  { path: 'dashboard', component: Dashboard, title: 'Dashboard | FitStride' },
+  { path: 'contact', component: Contact, title: 'Kontakt | FitStride' },
+  { path: '**', redirectTo: '' }
+];
