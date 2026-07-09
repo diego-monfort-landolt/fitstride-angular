a432b097bcf40247265c7efcb498d9e547a93e28
Diego-monfort-landot <diegomonfort1989@gmail.com>
2026-07-09 19:50:44 +0200

    rm blanc space

--- src/server.ts
+++ src/server.ts
@@ -61,7 +61,6 @@ if (isMainModule(import.meta.url) || process.env['pm_id']) {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
-
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
  */
